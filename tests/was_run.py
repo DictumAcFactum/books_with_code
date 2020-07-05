6bad75bcc721791a2f23e2b4f31475b66748d95f
Alex Levin <pinkfloydx20@gmail.com>
2020-07-05 15:05:03 +0300

    add set_up method

--- tests/was_run.py
+++ tests/was_run.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         method = getattr(self, self.name)
         method()
 
@@ -13,21 +17,28 @@ class WasRun(TestCase):
         super().__init__(name)
         self.was_run = False
 
+    def set_up(self):
+        self.was_run = False
+        self.was_set_up = True
+
     def test_method(self):
         self.was_run = True
 
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.test = WasRun('test_method')
+
     def test_running(self):
-        test = WasRun('test_method')
-        assert not test.was_run
-        test.run()
-        assert test.was_run
+        assert not self.test.was_run
+        self.test.run()
+        assert self.test.was_run
+
+    def test_set_up(self):
+        self.test.run()
+        assert self.test.was_set_up
 
 
 if __name__ == '__main__':
-    test = WasRun('test_method')
-    print(test.was_run)
-    test.run()
-    print(test.was_run)
     TestCaseTest('test_running').run()
+    TestCaseTest('test_set_up').run()
