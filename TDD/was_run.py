9787814fcc8e387d978b3621689fc55af34b5bd7
dictumacfactum <artofnoises0@gmail.com>
2020-10-15 22:09:54 +0300

    ch22 add err_count

--- TDD/was_run.py
+++ TDD/was_run.py
@@ -1,12 +1,16 @@
 class TestResult:
     def __init__(self):
         self.run_count = 0
+        self.err_count = 0
 
     def test_started(self):
         self.run_count += 1
 
+    def test_failed(self):
+        self.err_count += 1
+
     def summary(self):
-        return f'{self.run_count} run, 0 failed'
+        return f'{self.run_count} run, {self.err_count} failed'
 
 
 class TestCase:
@@ -25,7 +29,11 @@ class TestCase:
         result = TestResult()
         result.test_started()
         self.set_up()
-        exec(f'self.{self.name}()')
+        try:
+            exec(f'self.{self.name}()')
+        except Exception as e:
+            print(e)
+            result.test_failed()
         self.tear_down()
         return result
 
@@ -67,7 +75,14 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert '1 run, 1 failed' == result.summary()
 
+    def test_failed_result_formatting(self):
+        result = TestResult()
+        result.test_started()
+        result.test_failed()
+        assert '1 run, 1 failed' == result.summary()
+
 
 TestCaseTest('test_template_method').run()
 TestCaseTest('test_result').run()
 TestCaseTest('test_failed_result').run()
+TestCaseTest('test_failed_result_formatting').run()
